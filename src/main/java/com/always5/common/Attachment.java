77b9d2e01043e11f978e81550c5cdc8fc9bcd1c1
LLjay <julylee0724@gmail.com>
2024-04-12 17:18:33 +0900

    .

--- src/main/java/com/always5/common/Attachment.java
+++ src/main/java/com/always5/common/Attachment.java
@@ -2,5 +2,10 @@ package com.always5.common;
 
 public class Attachment {
 	private int fileNo;
-	private
+	private String changeName;
+	private String filePath;
+	private String reviewNo;
+	private String storeNo;
+	private String menuNo;
+	private String status;
 }
