223802222f25efce10248856c5dbcde76e56e031
LLjay <julylee0724@gmail.com>
2024-04-22 16:52:37 +0900

    .

--- src/main/java/com/always5/user/controller/UserLogoutController.java
+++ src/main/java/com/always5/user/controller/UserLogoutController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class UserLogoutController
  */
-@WebServlet("/Logout.ui")
+@WebServlet("/Logout.u")
 public class UserLogoutController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
