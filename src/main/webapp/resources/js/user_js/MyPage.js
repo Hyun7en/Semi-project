0f2e8d232dd25cf3a31df4fbe035236967cd62df
HyunJi <qjfhdnaos@gmail.com>
2024-04-24 00:51:37 +0900

    user view 수정 / 조현지

---
+++ src/main/webapp/resources/js/user_js/MyPage.js
@@ -0,0 +1,8 @@
+ function openUpdateName() { 
+	window.open("updatename.ui", "a", "width=754, height=900, left=100, top=50"); }
+	
+ function openUpdateId() { 
+	window.open("updateId.ui", "a", "width=754, height=900, left=100, top=50"); }
+	
+ function openUpdatePwd() { 
+	window.open("updatePwd.ui", "a", "width=754, height=900, left=100, top=50"); }
